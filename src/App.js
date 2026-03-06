3e7381aeaed95e001e8e29c4508a34a2a6b81cd3
Santosh Kumar <santoshmtelkur@gmail.com>
2026-03-06 22:32:01 +0530

    main

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
 
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
+         
       </Routes>
     </Router>
   );
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,36 @@
+import { useNavigate } from "react-router-dom";
+
+
+
+
 function Login() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Hello Login Page</h1>
+    <div  className="container">
+    <form   className="form">
+ <label>Email or Phone*</label>
+<input placeholder="Enter Email or Phone"/>
+
+<label>Password*</label>
+<input placeholder="Enter password"/>
+
+<button type="submit">Login</button>
+
+<button type="button"  >Forgot Password</button>
+<button type="button"     onClick={() => navigate("/")}>Create New Account</button>
+
+
+
+
+
+
+
+
+
+
+
+
+    </form>
     </div>
   );
 }
