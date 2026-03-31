451f9f6fb2fefc2494ff0eb0a240f4b84606163e
Santosh Kumar <santoshmtelkur@gmail.com>
2026-03-31 21:22:53 +0530

    logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
+import MyLogo from './MyLogo.png';
 
 function Header() {
   const navigate = useNavigate();
 
   return (
     <div className="navbar">
-      <h1 className="nav-logo" onClick={() => navigate("/")}>FindMyRide</h1>
+  <img className="logoheader" src={MyLogo} alt="logo" />
    <button className="nav-btn" onClick={() => navigate("/")}>Home</button>
       <button className="nav-btn">Contact Us</button>
       <button className="nav-btn">About Us</button>
